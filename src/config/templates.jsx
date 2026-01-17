1132ccd5c5f4f19faf174229337f444ee10d63d7
Samuel Aure <samuelaure@gmail.com>
2026-01-17 23:31:43 +0100

    fix: resolve bundling error by removing outdated template references in registry

--- src/config/templates.jsx
+++ src/config/templates.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import { MarketingV1 } from "../templates/MarketingV1.jsx";
-import { AttentionV1 } from "../templates/AttentionV1.jsx";
 import { ReelV1 } from "../templates/ReelV1.jsx";
 
 export const TemplateRegistry = (props) => {
   const { templateId } = props;
 
   switch (templateId) {
-    case "marketing-v1":
-      return <MarketingV1 {...props} />;
-    case "attention-v1":
-      return <AttentionV1 {...props} />;
     case "reel-v1":
       return <ReelV1 {...props} />;
     default:
